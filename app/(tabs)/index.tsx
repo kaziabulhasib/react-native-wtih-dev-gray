417e90191783e730b34df97c4ca66f43ea86b3ce
kaziabulhasib <hasibkazi420@gmail.com>
2025-01-24 11:09:47 +0530

    background image set: home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import React from "react";
-
+import icedCoffeImg from "@/assets/images/iced-coffee.png";
 const app = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Coffee shop</Text>
+      <ImageBackground
+        source={icedCoffeImg}
+        resizeMode='cover'
+        style={styles.image}>
+        <Text style={styles.text}>Coffee shop</Text>
+      </ImageBackground>
     </View>
   );
 };
@@ -15,6 +20,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
   },
   text: {
     color: "black",
