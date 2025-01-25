209f4576bb514282de23a4ee5923a3ffc8dbd6db
kaziabulhasib <hasibkazi420@gmail.com>
2025-01-25 22:14:32 +0530

    files recloate

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        {/* <Stack.Screen name='(tabs)' options={{ headerShown: false }} /> */}
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
         <Stack.Screen
           name='index'
           options={{ title: "Home", headerShown: false }}
